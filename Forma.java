f2a96f39ceca03fcebf9dade3a18bdfda5b8c1fd
Danpar000 <153896082+Danpar000@users.noreply.github.com>
2024-02-01 21:26:15 +0100

    Create Forma.java

---
+++ Forma.java
@@ -0,0 +1,19 @@
+public class Forma {
+
+    private String nombre = "";
+
+    public double area() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Forma{" +
+                "nombre='" + nombre + '\'' +
+                '}';
+    }
+
+    public Forma(String nombre) {
+        this.nombre = nombre;
+    }
+}
