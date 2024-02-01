7f538226effa498ff039eec27a94d327b1f78090
Danpar000 <153896082+Danpar000@users.noreply.github.com>
2024-02-01 21:26:41 +0100

    Create Rectangulo.java

---
+++ Rectangulo.java
@@ -0,0 +1,15 @@
+class Rectangulo extends Forma {
+    private double longitud;
+    private double ancho;
+
+    public Rectangulo(double longitud, double ancho) {
+        super("Rectángulo");
+        this.longitud = longitud;
+        this.ancho = ancho;
+    }
+
+    @Override
+    public double area() {
+        return longitud * ancho;
+    }
+}
